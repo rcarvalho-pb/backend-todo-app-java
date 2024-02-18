22d16f63ff247dcc0215751512481ae11ac331d6
Ramon Carvalho <rcarvalho.pb@gmail.com>
2024-02-18 11:54:20 -0300

    feat: create general not found exception

--- src/main/java/com/rcarvalho/todo/exceptions/TodoNotFoundException.java
+++ src/main/java/com/rcarvalho/todo/exceptions/TodoNotFoundException.java
@@ -2,7 +2,7 @@ package com.rcarvalho.todo.exceptions;
 
 import java.util.UUID;
 
-public class TodoNotFoundException extends RuntimeException{
+public class TodoNotFoundException extends NotFoundException{
     public TodoNotFoundException(UUID id) {
         super(String.format("To Do [%s] not found,", id.toString()));
     }
